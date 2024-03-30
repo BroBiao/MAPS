85c599e3f3d955c840cf003356baa365a29022c1
BroBiao <brobiao29@gmail.com>
2024-03-31 07:05:26 +0800

    first release version

--- maps.py
+++ maps.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
 import time
 
 
@@ -22,16 +22,26 @@ class Metamask(object):
 
     # check native token balance
     def get_native_balance(self):
-        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="代币"]'))).click()
+        try:
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="代币"]'))).click()
+        except ElementClickInterceptedException:
+            self.close_all_popup()
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="代币"]'))).click()
         balance = self.driver.find_element(By.XPATH, '//p[@data-testid="multichain-token-list-item-value"]')
         balance = balance.text.split(' ')[0]
         return float(balance)
 
     # add new account and copy address
     def add_new_account(self):
-        account_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 
-            'button.mm-box:nth-child(2)')))
-        account_btn.click()
+        try:
+            account_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 
+                'button.mm-box:nth-child(2)')))
+            account_btn.click()
+        except ElementClickInterceptedException:
+            self.close_all_popup()
+            account_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 
+                'button.mm-box:nth-child(2)')))
+            account_btn.click()
 
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, 
             '//button[normalize-space()="Add account or hardware wallet"]'))).click()
@@ -48,36 +58,45 @@ class Metamask(object):
 
     # switch to previous account
     def switch_account(self, index):
-        account_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.mm-box:nth-child(2)')))
+        try:
+            account_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 
+                'button.mm-box:nth-child(2)')))
+        except ElementClickInterceptedException:
+            self.close_all_popup()
+            account_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 
+                'button.mm-box:nth-child(2)')))
         account_btn.click()
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.multichain-account-menu-popover__list')))
         all_accounts = self.driver.find_elements(By.CSS_SELECTOR, 'div.multichain-account-menu-popover__list--menu-item')
         target_account = all_accounts[index]
         target_account.click()
-        self.close_all_popup()
 
     # conncect in metamask
     def connect_account(self, index=-1):
         self.close_all_popup()
-        try:
+        WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, 'choose-account-list__list')))
+        all_accounts = self.driver.find_elements(By.CLASS_NAME, 'choose-account-list__account')
+        if len(all_accounts) > 1:
             # select all accounts
             WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, 
                 '//input[@data-testid="choose-account-list-operate-all-check-box"]'))).click()
-        except TimeoutException:
-            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="下一步"]'))).click()
-            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="连接"]'))).click()
-            return
-        time.sleep(1)
-        # unselect all account
-        WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, 
-            '//input[@data-testid="choose-account-list-operate-all-check-box"]'))).click()
-        latest_account = self.driver.find_elements(By.CSS_SELECTOR, '.check-box.choose-account-list__list-check-box.far.fa-square')[index]
-        latest_account.click()
+            time.sleep(0.5)
+            # unselect all account
+            WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, 
+                '//input[@data-testid="choose-account-list-operate-all-check-box"]'))).click()
+            latest_account = self.driver.find_elements(By.CSS_SELECTOR, '.check-box.choose-account-list__list-check-box.far.fa-square')[index]
+            latest_account.click()
+        else:
+            pass
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="下一步"]'))).click()
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="连接"]'))).click()
 
     def import_token(self, token_address, token_name):
-        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="代币"]'))).click()
+        try:
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="代币"]'))).click()
+        except ElementClickInterceptedException:
+            self.close_all_popup()
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="代币"]'))).click()
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="添加代币"]'))).click()
         token_addr = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input.mm-box")))
         token_addr.send_keys(token_address)
@@ -88,22 +107,34 @@ class Metamask(object):
             WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="下一步"]'))).click()
             WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="导入"]'))).click()
 
-    def send_token(self, token_name):
-        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="代币"]'))).click()
+    def send_token(self, token_name, recv_addr='clipboard', amount='max'):
+        try:
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="代币"]'))).click()
+        except ElementClickInterceptedException:
+            self.close_all_popup()
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="代币"]'))).click()
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, f'//p[normalize-space()="{token_name}"]'))).click()
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, f'//p[normalize-space()="发送"]'))).click()
-        recv_addr = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".ens-input__wrapper__input")))
-        time.sleep(1)
-        recv_addr.send_keys(Keys.CONTROL + 'v')
+        recv_input = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".ens-input__wrapper__input")))
+        time.sleep(0.5)
+        if recv_addr == 'clipboard':
+            recv_input.send_keys(Keys.CONTROL + 'v')
+        else:
+            recv_input.send_keys(recv_addr)
         WebDriverWait(self.driver, 20).until(EC.invisibility_of_element_located((By.CLASS_NAME, "mm-box.loading-overlay")))
-        max_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".send-v2__amount-max")))
-        time.sleep(1)
-        max_btn.click()
-        time.sleep(2)
+        if amount == 'max':
+            max_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".send-v2__amount-max")))
+            time.sleep(1)
+            max_btn.click()
+            time.sleep(2)
+        else:
+            amount_input = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, 
+                '//input[@data-testid="currency-input"]')))
+            amount_input.send_keys(str(amount))
         WebDriverWait(self.driver, 20).until(EC.invisibility_of_element_located((By.CLASS_NAME, "mm-box.loading-overlay")))
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'//button[normalize-space()="下一步"]'))).click()
         WebDriverWait(self.driver, 20).until(EC.invisibility_of_element_located((By.CLASS_NAME, "mm-box.loading-overlay")))
-        time.sleep(1)
+        # time.sleep(1)
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'//button[normalize-space()="确认"]'))).click()
         while True:
             try:
@@ -121,9 +152,14 @@ class Metamask(object):
                 continue
 
     def disconnect_account(self):
-        WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, '//button[@aria-label="账户选项"]'))).click()
+        try:
+            WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, '//button[@aria-label="账户选项"]'))).click()
+        except ElementClickInterceptedException:
+            self.close_all_popup()
+            WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, '//button[@aria-label="账户选项"]'))).click()
         WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, 
             '//button[@data-testid="global-menu-connected-sites"]'))).click()
+        return
         WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, f'//a[normalize-space()="断开连接"]'))).click()
         WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="断开连接"]'))).click()
 
